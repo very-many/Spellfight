399d8a84b34d34b6f9891b72363160046d2cc677
pigeon-paddy <pxxdy1267@gmail.com>
2026-06-11 17:43:29 +0200

    Test Assets + SoundManager

---
+++ Assets/Scripts/Player/Sound/SoundValues.cs
@@ -0,0 +1,13 @@
+namespace SmallHedge.SoundManager
+{
+    public enum SoundType
+    {
+        Footstep,
+        Jump,
+        Land,
+        Slide,
+        Attack,
+        Death,
+        Hurt
+    }
+}
